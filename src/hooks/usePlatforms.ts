867f0107e1d6b1bffb5cdbb48b3c8ecf69206617
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-08-12 22:14:18 +0600

    Build platform Selector

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,11 @@
+import useData from "./useData";
+
+export interface Platform { 
+    id: number;
+    name: string;
+    slug: string;
+}
+
+const userPlatforms = () =>  useData<Platform>('/platforms/lists/parents');
+
+export default userPlatforms;
