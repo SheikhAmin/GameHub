738e4e843cc380b98b784498908f47ebd995fee3
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-08-15 10:04:43 +0600

    Build SortSelector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -13,7 +13,7 @@ const PlatformSelector = ({onSelectPlatform,selectedPlatform }:Props) => {
     return (
       <Menu>
         <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-          { selectedPlatform?.name || 'Platforms'}
+          { selectedPlatform?.name || 'Platforms '}
         </MenuButton>
         <MenuList>
                 {data.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{ platform.name}</MenuItem>)}
